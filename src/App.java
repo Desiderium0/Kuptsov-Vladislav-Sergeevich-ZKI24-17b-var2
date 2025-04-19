8cbd3db29d8e22d1c8d7851ff92c539e067ac42f
Desik <vlad.kuptsov2000@mail.ru>
2025-04-19 22:07:06 +0700

    q

--- src/App.java
+++ src/App.java
@@ -3,149 +3,150 @@ import java.util.Comparator;
 import java.util.Scanner;
 
 public class App {
-    private static ArrayList<PostalAddress> addresses = new ArrayList<>();
-    private static Scanner scanner = new Scanner(System.in);
+  private static ArrayList<PostalAddress> addresses = new ArrayList<>();
+  private static Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) {
-      showMenu();
-    }
+  public static void main(String[] args) {
+    showMenu();
+  }
 
-    private static void addEmptyAddress() {
-      addresses.add(new PostalAddress("", "", "", "", "", 0, 0, 0, 0));
-      System.out.println("Пустой адрес добавлен. Всего адресов: " + addresses.size());
-    }
+  private static void addEmptyAddress() {
+    addresses.add(new PostalAddress());
+    System.out.println("Пустой адрес добавлен. Всего адресов: " + addresses.size());
+  }
 
-    private static void addAddressWithData() {
-      System.out.println("\nВведите данные адреса.");
+  private static void addAddressWithData() {
+    System.out.println("\nВведите данные адреса.");
 
-      System.out.print("Страна: ");
-      String country = scanner.nextLine();
+    System.out.print("Страна: ");
+    String country = scanner.nextLine();
 
-      System.out.print("Регион: ");
-      String region = scanner.nextLine();
+    System.out.print("Регион: ");
+    String region = scanner.nextLine();
 
-      System.out.print("Город: ");
-      String city = scanner.nextLine();
+    System.out.print("Город: ");
+    String city = scanner.nextLine();
 
-      System.out.print("Почтовый индекс: ");
-      String postalCode = scanner.nextLine();
+    System.out.print("Почтовый индекс: ");
+    String postalCode = scanner.nextLine();
 
-      System.out.print("Улица: ");
-      String street = scanner.nextLine();
+    System.out.print("Улица: ");
+    String street = scanner.nextLine();
 
-      System.out.print("Номер дома: ");
-      int houseNumber = scanner.nextInt();
+    System.out.print("Номер дома: ");
+    int houseNumber = scanner.nextInt();
 
-      System.out.print("Квартира: ");
-      int apartment = scanner.nextInt();
+    System.out.print("Квартира: ");
+    int apartment = scanner.nextInt();
 
-      System.out.print("Подъезд: ");
-      int entrance = scanner.nextInt();
+    System.out.print("Подъезд: ");
+    int entrance = scanner.nextInt();
 
-      System.out.print("Этаж: ");
-      int floor = scanner.nextInt();
+    System.out.print("Этаж: ");
+    int floor = scanner.nextInt();
 
-      PostalAddress address = new PostalAddress
-          (country, region, city, postalCode, street, houseNumber, apartment, entrance, floor);
+    PostalAddress address = new PostalAddress (country, region, city, postalCode, street, houseNumber, apartment, entrance, floor);
       addresses.add(address);
       System.out.println("Адрес добавлен. Всего адресов: " + addresses.size());
+  }
+ 
+  private static void editAddress() {
+    if (addresses.isEmpty()) {
+      System.out.println("Список адресов пуст.");
+      return;
     }
 
-    private static void editAddress() {
-        if (addresses.isEmpty()) {
-          System.out.println("Список адресов пуст.");
-          return;
-        }
+    System.out.print("Введите индекс адреса для редактирования (0-" + (addresses.size() - 1) + "): ");
+    String indexStr = scanner.nextLine();
+    int index = -1;
 
-      System.out.print("Введите индекс адреса для редактирования (0-" + (addresses.size() - 1) + "): ");
-      String indexStr = scanner.nextLine();
-      int index = -1;
+    index = Integer.parseInt(indexStr);
 
-      index = Integer.parseInt(indexStr);
-
-        if (index < 0 || index >= addresses.size()) {
-          System.out.println("Неверный индекс адреса.");
-          return;
-        }
-
-      PostalAddress address = addresses.get(index);
-
-      System.out.println("\nВыберите поле для редактирования:");
-      System.out.println("1. Страна");
-      System.out.println("2. Регион");
-      System.out.println("3. Город");
-      System.out.println("4. Почтовый индекс");
-      System.out.println("5. Улица");
-      System.out.println("6. Номер дома");
-      System.out.println("7. Квартира");
-      System.out.println("8. Подъезд");
-      System.out.println("9. Этаж");
-      System.out.print("Ваш выбор: ");
-
-      int fieldChoice = scanner.nextInt();
-      System.out.print("Введите новое значение: ");
-      String newValueStr = "";
-      double newValueNum = 0;
-
-        if (fieldChoice >= 1 && fieldChoice <= 5) {
-          newValueStr = scanner.next();
-          scanner.nextLine();
-        } else if (fieldChoice >= 6 && fieldChoice <= 9) {
-          newValueNum = scanner.nextInt();
-        }
-
-        switch (fieldChoice) {
-          case 1:
-              address.setCountry(newValueStr);
-              break;
-          case 2:
-              address.setRegion(newValueStr);
-              break;
-          case 3:
-              address.setCity(newValueStr);
-              break;
-          case 4:
-              address.setPostalCode(newValueStr);
-              break;
-          case 5:
-              address.setStreet(newValueStr);
-              break;
-          case 6:
-              address.setHouseNumber((int) newValueNum);
-              break;
-          case 7:
-              address.setApartment((int) newValueNum);
-              break;
-          case 8:
-              address.setFloor(newValueNum);
-              break;
-          default:
-              System.out.println("Неверный выбор поля.");
-              return;
-        }
-      System.out.println("Адрес успешно обновлен.");
+    if (index < 0 || index >= addresses.size()) {
+      System.out.println("Неверный индекс адреса.");
+      return;
     }
 
-    private static void showAllAddresses() {
-        if (addresses.isEmpty()) {
-          System.out.println("Список адресов пуст.");
-          return;
-        }
+    PostalAddress address = addresses.get(index);
 
-      System.out.println("\n=== Список всех адресов ===");
+    System.out.println("\nВыберите поле для редактирования:");
+    System.out.println("1. Страна");
+    System.out.println("2. Регион");
+    System.out.println("3. Город");
+    System.out.println("4. Почтовый индекс");
+    System.out.println("5. Улица");
+    System.out.println("6. Номер дома");
+    System.out.println("7. Квартира");
+    System.out.println("8. Подъезд");
+    System.out.println("9. Этаж");
+    System.out.print("Ваш выбор: ");
+
+    int fieldChoice = scanner.nextInt();
+    System.out.print("Введите новое значение: ");
+    String newValueStr = "";
+    double newValueNum = 0;
+
+    if (fieldChoice >= 1 && fieldChoice <= 5) {
+      newValueStr = scanner.next();
+      scanner.nextLine();
+    } else if (fieldChoice >= 6 && fieldChoice <= 9) {
+      newValueNum = scanner.nextInt();
+    }
+
+    switch (fieldChoice) {
+      case 1:
+        address.setCountry(newValueStr);
+        break;
+      case 2:
+        address.setRegion(newValueStr);
+        break;
+      case 3:
+        address.setCity(newValueStr);
+        break;
+      case 4:
+        address.setPostalCode(newValueStr);
+        break;
+      case 5:
+        address.setStreet(newValueStr);
+        break;
+      case 6:
+        address.setHouseNumber((int) newValueNum);
+        break;
+      case 7:
+        address.setApartment((int) newValueNum);
+        break;
+      case 8:
+        address.setEntrance((int) newValueNum);
+        break;
+      case 9:
+        address.setFloor(newValueNum);
+        break;
+      default:
+        System.out.println("Неверный выбор поля.");
+        return;
+      }
+    }
+
+  private static void showAllAddresses() {
+    if (addresses.isEmpty()) {
+      System.out.println("Список адресов пуст.");
+      return;
+    }
+
+    System.out.println("\n=== Список всех адресов ===");
       
-        for (int i = 0; i < addresses.size(); i++) {
-          System.out.println("\nАдрес #" + i + ":");
-          System.out.println(addresses.get(i).getFullAddress());
-          System.out.println("Тип доставки: " + addresses.get(i).getDeliveryType());
-        }
+    for (int i = 0; i < addresses.size(); i++) {
+      System.out.println("\nАдрес #" + i + ":");
+      System.out.println(addresses.get(i).getFullAddress());
+      System.out.println("Тип доставки: " + addresses.get(i).getDeliveryType());
     }
+  }
 
     private static void sortAddresses() {
-        if (addresses.isEmpty()) {
+      if (addresses.isEmpty()) {
           System.out.println("Список адресов пуст.");
           return;
-        }
+      }
 
       System.out.println("\nВыберите поле для сортировки:");
       System.out.println("1. Страна");
@@ -159,69 +160,69 @@ public class App {
 
       Comparator<PostalAddress> comparator = null;
 
-        switch (sortChoice) {
-          case "1":
-              comparator = Comparator.comparing(PostalAddress::getCountry);
-              break;
-          case "2":
-              comparator = Comparator.comparing(PostalAddress::getRegion);
-              break;
-          case "3":
-              comparator = Comparator.comparing(PostalAddress::getCity);
-              break;
-          case "4":
-              comparator = Comparator.comparing(PostalAddress::getPostalCode);
-              break;
-          case "5":
-             comparator = Comparator.comparing(PostalAddress::getStreet);
-              break;
-          case "6":
-              comparator = Comparator.comparing(PostalAddress::getHouseNumber);
-              break;
-          default:
-              System.out.println("Неверный выбор поля для сортировки.");
-              return;
-        }
-      addresses.sort(comparator);
-      System.out.println("Адреса отсортированы.");
+      switch (sortChoice) {
+        case "1":
+          comparator = Comparator.comparing(PostalAddress::getCountry);
+          break;
+        case "2":
+          comparator = Comparator.comparing(PostalAddress::getRegion);
+          break;
+        case "3":
+          comparator = Comparator.comparing(PostalAddress::getCity);
+          break;
+        case "4":
+          comparator = Comparator.comparing(PostalAddress::getPostalCode);
+          break;
+        case "5":
+          comparator = Comparator.comparing(PostalAddress::getStreet);
+          break;
+        case "6":
+          comparator = Comparator.comparing(PostalAddress::getHouseNumber);
+          break;
+        default:
+          System.out.println("Неверный выбор поля для сортировки.");
+          return;
     }
+    addresses.sort(comparator);
+    System.out.println("Адреса отсортированы.");
+  }
 
-    private static void showMenu() {
-      while (true) {
-          System.out.println("\n=== Меню управления почтовыми адресами ===");
-          System.out.println("1. Добавить пустой адрес");
-          System.out.println("2. Добавить адрес с данными");
-          System.out.println("3. Редактировать адрес");
-          System.out.println("4. Показать все адреса");
-          System.out.println("5. Сортировать адреса");
-          System.out.println("6. Выход");
-          System.out.print("Выберите пункт меню: ");
+  private static void showMenu() {
+    while (true) {
+      System.out.println("\n=== Меню управления почтовыми адресами ===");
+      System.out.println("1. Добавить пустой адрес");
+      System.out.println("2. Добавить адрес с данными");
+      System.out.println("3. Редактировать адрес");
+      System.out.println("4. Показать все адреса");
+      System.out.println("5. Сортировать адреса");
+      System.out.println("6. Выход");
+      System.out.print("Выберите пункт меню: ");
 
-          String choice = scanner.next();
-          scanner.nextLine();
+      String choice = scanner.next();
+      scanner.nextLine();
 
-          switch (choice) {
-              case "1":
-                  addEmptyAddress();
-                  break;
-              case "2":
-                  addAddressWithData();
-                  break;
-              case "3":
-                  editAddress();
-                  break;
-              case "4":
-                  showAllAddresses();
-                  break;
-              case "5":
-                  sortAddresses();
-                  break;
-              case "6":
-                  System.out.println("Программа завершена.");
-                  return;
-              default:
-                  System.out.println("Неверный выбор. Попробуйте снова.");
-          }
-        }
+      switch (choice) {
+        case "1":
+          addEmptyAddress();
+          break;
+        case "2":
+          addAddressWithData();
+          break;
+        case "3":
+          editAddress();
+          break;
+        case "4":
+          showAllAddresses();
+          break;
+        case "5":
+          sortAddresses();
+          break;
+        case "6":
+          System.out.println("Программа завершена.");
+          return;
+        default:
+          System.out.println("Неверный выбор. Попробуйте снова.");
+      }
     }
+  }
 }
--- src/PostalAddress.java
+++ src/PostalAddress.java
@@ -1,118 +1,151 @@
 public class PostalAddress {
-    private String country;
-    private String region;
-    private String city;
-    private String postalCode;
-    private String street;
-    private int houseNumber;
-    private int apartment;
-    public int entrance;
-    private double floor;
+  private String country;
+  private String region;
+  private String city;
+  private String postalCode;
+  private String street;
+  private int houseNumber;
+  private int apartment;
+  public int entrance;
+  private double floor;
 
-    public PostalAddress() {
-    }
+  public PostalAddress() { }
 
-    public PostalAddress(String country, String region, String city,
-          String postalCode, String street, int houseNumber,
-          int apartment, int entrance, double floor) {
-        this.country = country;
-        this.region = region;
-        this.city = city;
-        this.postalCode = postalCode;
-        this.street = street;
-        this.houseNumber = houseNumber;
-        this.apartment = apartment;
-        this.entrance = entrance;
-        this.floor = floor;
-    }
+  public PostalAddress(String country, String region, String city,
+      String postalCode, String street, int houseNumber,
+      int apartment, int entrance, double floor) {
+    this.country = country;
+    this.region = region;
+    this.city = city;
+    this.postalCode = postalCode;
+    this.street = street;
+    this.houseNumber = houseNumber;
+    this.apartment = apartment;
+    this.entrance = entrance;
+    this.floor = floor;
+  }
 
-    public String getCountry() {
-      return country;
-    }
+  public String getCountry() {
+    return country;
+  }
 
-    public void setCountry(String country) {
-      this.country = country;
-    }
+  public void setCountry(String country) {
+    this.country = country;
+    System.out.println("Страна успешно обновлена.");
+  }
 
-    public String getRegion() {
-      return region;
-    }
+  public String getRegion() {
+    return region;
+  }
 
-    public void setRegion(String region) {
-      this.region = region;
-    }
+  public void setRegion(String region) {
+    this.region = region;
+    System.out.println("Регион успешно обновлен.");
+  }
 
-    public String getCity() {
-      return city;
-    }
+  public String getCity() {
+    return city;
+  }
 
-    public void setCity(String city) {
-      this.city = city;
-    }
+  public void setCity(String city) {
+    this.city = city;
+    System.out.println("Город успешно обновлен.");
+  }
 
-    public String getPostalCode() {
-      return postalCode;
-    }
+  public String getPostalCode() {
+    return postalCode;
+  }
 
-    public void setPostalCode(String postalCode) {
-      this.postalCode = postalCode;
-    }
+  public void setPostalCode(String postalCode) {
+    this.postalCode = postalCode;
+    System.out.println("Почтовый индекс успешно обновлен.");
+  }
 
-    public String getStreet() {
-      return street;
-    }
+  public String getStreet() {
+    return street;
+  }
 
-    public void setStreet(String street) {
-      this.street = street;
-    }
+  public void setStreet(String street) {
+    this.street = street;
+    System.out.println("Улица успешно обновлен.");
+  }
 
-    public int getHouseNumber() {
-      return houseNumber;
-    }
+  public int getHouseNumber() {
+    return houseNumber;
+  }
 
-    public void setHouseNumber(int houseNumber) {
+  public void setHouseNumber(int houseNumber) {
+    if (houseNumber >= 0) {
       this.houseNumber = houseNumber;
+      System.out.println("Номер дома успешно обновлен.");
     }
-
-    public int getApartment() {
-      return apartment;
+    else {
+      System.out.println("Не веррно введено значение, не должно быть отрицательным");
     }
+  }
 
-    public void setApartment(int apartment) {
+  public int getApartment() {
+    return apartment;
+  }
+
+  public void setApartment(int apartment) {
+    if (apartment >= 0) {
       this.apartment = apartment;
+      System.out.println("Квартира успешно обновлена.");
     }
-
-    public double getFloor() {
-      return floor;
+    else {
+      System.out.println("Не веррно введено значение, не должно быть отрицательным");
     }
+  }
 
-    public void setFloor(double floor) {
+  public double getFloor() {
+    return floor;
+  }
+
+  public void setFloor(double floor) {
+    if (floor >= 0) {
       this.floor = floor;
+      System.out.println("Этаж успешно обновлен.");
+    }
+    else {
+      System.out.println("Не веррно введено значение, не должно быть отрицательным");
+    }
+  }
+
+  public void setEntrance(int entrance) {
+    if (entrance >= 0) {
+      this.entrance = entrance;
+      System.out.println("Подъезд успешно обновлен.");
+    }
+    else {
+      System.out.println("Не веррно введено значение, не должно быть отрицательным");
+    }
+  }
+
+  public String getFullAddress() {
+    return String.format(
+      "Страна: %s\nРегион: %s\nГород: %s\nУлица: %s\nПодъезд: %s\nЭтаж: %s\nНомер дома: %s\nКвартира: %s\nПочтовый индекс: %s",
+      country, region, city, street, entrance, floor, houseNumber, apartment != 0 ? apartment : "", postalCode);
     }
 
-    public String getFullAddress() {
-      return String.format(
-          "Страна: %s\nРегион: %s\nГород: %s\nУлица: %s\nПодъезд: %s\nЭтаж: %s\nНомер дома: %s\nКвартира: %s\nПочтовый индекс: %s",
-          country, region, city, street, entrance, floor, houseNumber, apartment != 0 ? apartment : "",
-          postalCode);
+  public String getDeliveryType() {
+    if (country != null && postalCode != null) {
+      // Если адрес в другой стране — международная доставка
+      if (!country.equalsIgnoreCase("Россия")) {
+        return "Международная";
+      }
+
+      // Если почтовый индекс начинается с "1" — экспресс-доставка (условно)
+      if (postalCode.startsWith("1")) {
+        return "Экспресс";
+      }
+    }
+  
+    // Если нет номера квартиры — это бизнес-адрес (крупногабаритная доставка)
+    if (apartment == 0) {
+      return "Крупногабаритная";
     }
 
-    public String getDeliveryType() {
-        // Если адрес в другой стране — международная доставка
-        if (!country.equalsIgnoreCase("Россия")) {
-          return "Международная";
-        }
-
-        // Если почтовый индекс начинается с "1" — экспресс-доставка (условно)
-        if (postalCode.startsWith("1")) {
-          return "Экспресс";
-        }
-
-        // Если нет номера квартиры — это бизнес-адрес (крупногабаритная доставка)
-        if (apartment == 0) {
-          return "Крупногабаритная";
-        }
-
-        return "Стандартная";
-    }
+    return "Стандартная";
+  }
 }
